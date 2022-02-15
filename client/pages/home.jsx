94dc865c23229d1dc0975bf18d01ee62948dc3e1
edtan094 <etan094@gmail.com>
2022-02-15 09:48:16 -0800

    Attempting to use bootstrap.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -3,41 +3,6 @@ import React from 'react';
 export default function Home(props) {
   return (
     <>
-      <div className="container">
-        <div className="row align-items-start">
-          <div className="col">
-            One of three columns
-          </div>
-          <div className="col">
-            One of three columns
-          </div>
-          <div className="col">
-            One of three columns
-          </div>
-        </div>
-        <div className="row align-items-center">
-          <div className="col">
-            One of three columns
-          </div>
-          <div className="col">
-            One of three columns
-          </div>
-          <div className="col">
-            One of three columns
-          </div>
-        </div>
-        <div className="row align-items-end">
-          <div className="col">
-            One of three columns
-          </div>
-          <div className="col">
-            One of three columns
-          </div>
-          <div className="col">
-            One of three columns
-          </div>
-        </div>
-      </div>
     </>
   );
 }
