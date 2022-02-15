c750e89a48ea92aecf8902b1313e99b4c175ed3c
edtan094 <etan094@gmail.com>
2022-02-14 17:58:34 -0800

    devised code with index.js so that when the client submit preferences, server does app.get '/yelp/:preference/:location'

--- server/index.js
+++ server/index.js
@@ -1,12 +1,34 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 require('dotenv/config');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
-
 const app = express();
+function random(length) {
+  return Math.floor(Math.random() * length);
+}
 
 app.use(staticMiddleware);
+
+const body = {
+  method: 'GET',
+  headers: {
+    Authorization:
+      process.env.YELP_AUTHORIZATION
+  }
+};
+app.get('/yelp/:preference/:location', (req, res) => {
+  const { location } = req.params;
+  const { preference } = req.params;
+  fetch(`https://api.yelp.com/v3/businesses/search?categories=${preference}&location=${location}`, body)
+    .then(response => response.json())
+    .then(data => {
+      const randomNumber = random(data.businesses.length);
+      return data.businesses[randomNumber];
+    })
+    .catch(error => console.error(error));
+});
 
 app.use(errorMiddleware);
 
