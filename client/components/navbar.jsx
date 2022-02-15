711189f3f4056de2b9d9f50c2860af2e94aeb587
edtan094 <etan094@gmail.com>
2022-02-15 14:57:34 -0800

    Added navbar, page-container, and home page component.

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -7,7 +7,10 @@ export default class Navbar extends React.Component {
         <div className="container navbar row align-center">
             <div className="column-full">
               <div className="row white-text justify-space-between">
-                <a className='font-theme heading'>NomNom</a>
+              <div className='row align-center'>
+                <a className='font-theme heading padding-right'>NomNom</a>
+                <img className='logo' src='./images/chocolate-chip-cookie-biscuits-clip-art-cookies-removebg-preview.png'></img>
+              </div>
                 <div className="row justify-end align-center">
                   <a className='padding-right font-theme'>Home</a>
                   <a className='font-theme'>Try Again</a>
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 export default function Home(props) {
   return (
-    <>
-    </>
+    <div className='row center-of-page vh-height'>
+      <form method='get' className='home-page-form'>
+        <div className='row'>
+          <label className='font-theme home-page-font-size padding-right'>I want to eat</label>
+          <input className='input-params' placeholder='          type of food...' type="text" htmlFor='eat' id='eat' required></input>
+        </div>
+        <div className='row padding-top'>
+          <label className='font-theme home-page-font-size padding-right'>at</label>
+          <input className='input-params' placeholder="          city name..." type="text" htmlFor='at' id='at' required></input>
+        </div>
+        <div className='row justify-end'>
+          <input className='search-button' type="submit" value="SEARCH"></input>
+        </div>
+      </form>
+    </div>
   );
 }
