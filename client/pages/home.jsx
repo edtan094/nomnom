428e032d482538db51997c5978fd2c99cdf27787
edtan094 <etan094@gmail.com>
2022-02-18 11:13:28 -0800

    Replaced input with select element and added options.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -31,7 +31,62 @@ export default class Home extends React.Component {
         <form onSubmit={this.handleSubmit} method='get' className='home-page-form'>
           <div className='row'>
             <label className='font-theme home-page-font-size padding-right'>I want to eat</label>
-            <input onChange={this.handleInputPreference} value={this.state.preference} className='input-params' placeholder='type of food...' type="text" htmlFor='eat' id='eat' required></input>
+            <select onChange={this.handleInputPreference} value={this.state.preference} name='preferences'>
+              <option value="">None</option>
+              <option value="tradamerican">American</option>
+              <option value="brazilian">Brazilian</option>
+              <option value="breakfast_brunch">Brunch</option>
+              <option value="burgers">Burgers</option>
+              <option value="cafes">Cafes</option>
+              <option value="chicken_wings">Chicken Wings</option>
+              <option value="chinese">Chinese</option>
+              <option value="comfortfood">Comfort Food</option>
+              <option value="hotdogs">Fast Food</option>
+              <option value="filipino">Filipino</option>
+              <option value="fishnchips">Fish & Chips</option>
+              <option value="french">French</option>
+              <option value="gastropubs">Gastropubs</option>
+              <option value="german">German</option>
+              <option value="greek">Greek</option>
+              <option value="halal">Halal</option>
+              <option value="hawaiian">Hawaiian</option>
+              <option value="hkcafe">Hong Kong Style Cafe</option>
+              <option value="hotpot">Hot Pot</option>
+              <option value="indpak">Indian</option>
+              <option value="irish">Irish</option>
+              <option value="italian">Italian</option>
+              <option value="japanese">Japanese</option>
+              <option value="korean">Korean</option>
+              <option value="latin">Latin American</option>
+              <option value="malaysian">Malaysian</option>
+              <option value="mediterranean">Mediterranean</option>
+              <option value="mexican">Mexican</option>
+              <option value="mideastern">Middle Eastern</option>
+              <option value="noodles">Noodles</option>
+              <option value="pakistani">Pakistani</option>
+              <option value="persian">Persian/Iranian</option>
+              <option value="peruvian">Peruvian</option>
+              <option value="pizza">Pizza</option>
+              <option value="polish">Polish</option>
+              <option value="portuguese">Portuguese</option>
+              <option value="poutineries">Poutineries</option>
+              <option value="russian">Russian</option>
+              <option value="salad">Salad</option>
+              <option value="sandwiches">Sandwiches</option>
+              <option value="singaporean">Singaporean</option>
+              <option value="soup">Soup</option>
+              <option value="spanish">Spanish</option>
+              <option value="steak">SteakHouses</option>
+              <option value="sushi">Sushi Bars</option>
+              <option value="taiwanese">Taiwanese</option>
+              <option value="tapasmallplates">Tapas/Small Plates</option>
+              <option value="thai">Thai</option>
+              <option value="turkish">Turkish</option>
+              <option value="vegan">Vegan</option>
+              <option value="vegetarian">Vegetarian</option>
+              <option value="vietnamese">Vietnamese</option>
+              <option value="waffles">Waffles</option>
+            </select>
           </div>
           <div className='row padding-top'>
             <label className='font-theme home-page-font-size padding-right'>at</label>
