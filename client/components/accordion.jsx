bdc3213d2f4e472a73e7b06f1941b1c11473c5c6
edtan094 <etan094@gmail.com>
2022-02-21 12:37:28 -0800

    Styled the accordion with CSS so that it matches the theme of the page

--- client/components/accordion.jsx
+++ client/components/accordion.jsx
@@ -18,17 +18,17 @@ export default class Accordion extends React.Component {
 
   render() {
     return (
-      <div className='row justify-center'>
+      <div className='row justify-center accordion-padding-top'>
         <div className='row direction-column width-accordion'>
           {this.props.reviews.map((reviews, index) => {
             const handleClick = () => this.setOpenReview(index);
             return (
               <div key={index}>
-                <div onClick={handleClick} className="row justify-space-between border-bottom">{reviews.review.user.name}
-                <i className="fa-solid fa-caret-down"></i></div>
-                  <div>
+                <div onClick={handleClick} className={index === this.state.openReview ? 'row justify-space-between align-center' : 'row justify-space-between border-bottom align-center' }><h4 className="roboto-font">{reviews.review.user.name}</h4>
+                  {index === this.state.openReview ? <i className="fa-solid fa-caret-up"></i> : <i className="fa-solid fa-caret-down"></i>}</div>
+                <div className={index === this.state.openReview ? 'border-bottom' : ''}>
                   {index === this.state.openReview && <div className='row'>
-                    <div><img className='review-image' src={reviews.review.user.image_url}></img></div>{reviews.review.text}
+                    <div><img className='review-image' src={reviews.review.user.image_url}></img></div><span className='roboto-font'>{reviews.review.text}</span>
                     </div>}
                   </div>
               </div>
