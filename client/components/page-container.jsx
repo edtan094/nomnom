32ff05b0b08de8fe35941385f54aac50960e1258
edtan094 <etan094@gmail.com>
2022-02-23 12:36:49 -0800

    Completed design-update-2.

--- client/components/page-container.jsx
+++ client/components/page-container.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function PageContainer({ children }) {
   return (
     <div>
-      <div className="container margin-bottom">
+      <div className="container margin-top-10 margin-bottom">
         {children}
       </div>
     </div>
