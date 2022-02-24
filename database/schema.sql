b4ef0440712936bd678326515b1f06b5c80996cd
edtan094 <etan094@gmail.com>
2022-02-24 10:25:42 -0800

    Updated schema.sql and data.sql.  Created a database called finalPorject and imported the files to the database

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,12 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+create table "public"."users" (
+  "userId"         serial,
+  "username"       text           not null,
+  "hashedPassword" text           not null,
+  "createdAt"      timestamptz(6) not null default now(),
+  primary key ("userId"),
+  unique ("username")
+);
