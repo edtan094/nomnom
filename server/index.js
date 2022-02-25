6cd2a48a26476e55b6e5b45dcc9c5586cdce3d37
edtan094 <etan094@gmail.com>
2022-02-24 18:31:19 -0800

    Updated index.js so that it can handle a sign in request with app.post.  Included errors if the user does not have the correct credentials.

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 const ClientError = require('./client-error');
 const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
 const pg = require('pg');
 const app = express();
 const client = require('twilio')(`${process.env.TWILIO_SID}`, `${process.env.TWILIO_AUTH}`);
@@ -46,6 +47,39 @@ app.post('/api/auth/sign-up', (req, res, next) => {
       res.status(200).json(user);
     })
     .catch(error => next(error));
+});
+
+app.post('/api/auth/sign-in', (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new ClientError(401, 'invalid login');
+  }
+  const sql = `
+    select "userId",
+           "hashedPassword"
+      from "users"
+     where "username" = $1
+  `;
+  const params = [username];
+  db.query(sql, params)
+    .then(result => {
+      const [user] = result.rows;
+      if (!user) {
+        throw new ClientError(401, 'invalid login');
+      }
+      const { userId, hashedPassword } = user;
+      return argon2
+        .verify(hashedPassword, password)
+        .then(isMatching => {
+          if (!isMatching) {
+            throw new ClientError(401, 'invalid login');
+          }
+          const payload = { userId, username };
+          const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+          res.json({ token, user: payload });
+        });
+    })
+    .catch(err => next(err));
 });
 
 const body = {
